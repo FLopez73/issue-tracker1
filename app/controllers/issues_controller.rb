876f792960536e00bf55001aa2ea92a667f6d389
flopez73 <flopez730@gmail.com>
2023-11-08 16:50:25 -0800

    working on views

---
+++ app/controllers/issues_controller.rb
@@ -0,0 +1,26 @@
+class IssuesController < ApplicationController
+
+    def index 
+        @issues = Issue.all
+        @issue = Issue.new
+    end
+
+
+    def create 
+        @issue = Issue.new(issue_params)
+        
+        if @issue.save 
+            redirect_to issues_url, notice: "Issue was successfully created."
+        else 
+            render :issue, status: :unprocessable_entity
+        end
+      end
+    end
+
+
+    private 
+
+    def issue_params
+        params.require(:issue).permit(:title, :description, :priority, :assignee, :status)
+    end
+
