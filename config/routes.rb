e63f555ddcf8749a4f28fa50c4fb5a78ccaec580
flopez73 <flopez730@gmail.com>
2023-11-09 15:35:34 -0800

    full crud working

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
  
   post "issues/:id/toggle", to: "issues#toggle"
-
+  delete "issues/:id" => "issues#destroy"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
